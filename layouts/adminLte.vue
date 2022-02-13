edfcd2cf6f8a58a6149552e53012770691a80e6b
EdmundoEsquivel <edmundoesquivel@live.com>
2022-02-13 09:26:55 -0600

    nuxt con los componentes de adminlte y funcionando index

---
+++ layouts/adminLte.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Preloader />
+    <NavBar />
+    <SideBar />
+    <nuxt class="content-wrapper" />
+    <Footer />
+    <ControlSidebar />
+  </div>
+</template>
+
+<script>
+import Preloader from '../components/pages/Preloader'
+import NavBar from '../components/pages/NavBar'
+import SideBar from '../components/pages/SideBar'
+import Footer from '../components/pages/Footer'
+import ControlSidebar from '../components/pages/ControlSidebar.vue'
+export default {
+  components: {
+    NavBar, SideBar, Footer, Preloader, ControlSidebar
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,113 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  // mode: 'universal',
+  telemetry: false,
+  components: true,
+  jquery: true,
+  
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+  
+ 
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/plugins/fontawesome-free/css/all.min.css' },
+      { rel: 'stylesheet', href: 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css' },
+      { rel: 'stylesheet', href: '/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css' },
+      { rel: 'stylesheet', href: '/plugins/icheck-bootstrap/icheck-bootstrap.min.css' },
+      { rel: 'stylesheet', href: '/plugins/jqvmap/jqvmap.min.css' },
+      { rel: 'stylesheet', href: '/dist/css/adminlte.min.css' },
+      { rel: 'stylesheet', href: '/plugins/overlayScrollbars/css/OverlayScrollbars.min.css' },
+      { rel: 'stylesheet', href: '/plugins/daterangepicker/daterangepicker.css' },
+      { rel: 'stylesheet', href: '/plugins/summernote/summernote-bs4.css' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700' }
+    ],
+    script: [
+      { src: '/plugins/jquery/jquery.min.js', body: true },
+      { src: '/plugins/jquery-ui/jquery-ui.min.js', body: true },
+      { src: '/plugins/bootstrap/js/bootstrap.bundle.min.js', body: true },
+      { src: '/plugins/chart.js/Chart.min.js', body: true },
+      { src: '/plugins/sparklines/sparkline.js', body: true },
+      { src: '/plugins/jqvmap/jquery.vmap.min.js', body: true },
+      { src: '/plugins/jqvmap/maps/jquery.vmap.usa.js', body: true },
+      { src: '/plugins/jquery-knob/jquery.knob.min.js', body: true },
+      { src: '/plugins/moment/moment.min.js', body: true },
+      { src: '/plugins/daterangepicker/daterangepicker.js', body: true },
+      { src: '/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js', body: true },
+      { src: '/plugins/summernote/summernote-bs4.min.js', body: true },
+      { src: '/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js', body: true },
+      { src: '/dist/js/adminlte.js', body: true },
+      { src: '/dist/js/demo.js', body: true },
+      { src: '/dist/js/pages/dashboard.js', body: true }
+    ],
+    bodyAttrs: {
+      class: 'hold-transition sidebar-mini layout-fixed'
+    }
+
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    // 'admin-lte/plugins/fontawesome-free/css/all.min.css',
+    // 'admin-lte/dist/css/adminlte.css',
+  ],
+
+ 
+
+  
+
+  
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
+    
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  
+  /*
+  ** Build configuration
+  */
+
+  
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
