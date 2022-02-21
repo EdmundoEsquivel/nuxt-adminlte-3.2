4e4cc4015e12a38fb878c0ed658f28486f7faded
EdmundoEsquivel <edmundoesquivel@live.com>
2022-02-20 23:39:11 -0600

    integre sequelize, mysql2, dotenv, express-validator y body-parser

---
+++ pages/login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="top-content">
+      <div class="inner-bg">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-sm-offset-3 form-box">
+              <div class="form-top">
+                <div class="form-top-left">
+                  <h1 style="margin-bottom: -5px">
+                    <strong>Vamos a ganar</strong>
+                  </h1>
+                  <p>SUI - Sistema Unico de Información</p>
+                </div>
+                <div class="form-top-right">
+                  <i class="fa fa-lock"></i>
+                </div>
+              </div>
+              <div class="form-bottom">
+                <div class="err" id="add_err" style="color: #fff"></div>
+                <form
+                  role="form"
+                  @submit.prevent="submitForm"
+                  class="login-form"
+                >
+                  <div class="form-group">
+                    <label class="sr-only" for="form-username">Username</label>
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Nombre de Usuario"
+                      class="form-username form-control"
+                      id="email"
+                      v-model="email"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label class="sr-only" for="form-password">Password</label>
+                    <input
+                      type="password"
+                      name="pass"
+                      placeholder="Contraseña"
+                      class="form-password form-control"
+                      id="password"
+                      v-model="password"
+                    />
+                  </div>
+                  <button type="submit" class="btn" id="login">
+                    Ingresar!
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "loginTemplate",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log("esta dando click");
+      this.$axios
+        .post('/api/users', {
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  components: {},
+};
+</script>
---
+++ static/assets/js/scripts.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+	
+	$.backstretch([
+      "assets/img/backgrounds/1.jpg"
+    , "assets/img/backgrounds/2.jpg"
+    , "assets/img/backgrounds/3.jpg"
+	, "assets/img/backgrounds/4.jpg"
+	, "assets/img/backgrounds/5.jpg"
+	, "assets/img/backgrounds/6.jpg"
+  ], {duration: 3000, fade: 750});
+	
+	
+	
+    
+ 
+
+    
+    
+});
